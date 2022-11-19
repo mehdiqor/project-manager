db4e486492c1a56e74b105d97228bbc74fc2f59a
mehdiqor <mehdighorbanin@gmail.com>
2022-11-19 19:43:14 +0330

    project section

--- app/http/controllers/project.controller.js
+++ app/http/controllers/project.controller.js
@@ -29,8 +29,40 @@ class ProjectConttroller{
             next(error)
         }
     }
-    getProjectById(){
-
+    async findProject(projectID, owner){
+        const project = await ProjectModel.findOne({owner, _id: projectID})
+        if(!project) throw {status: 404, message: "پروژه ای یافت نشد"}
+        return project
+    }
+    async getProjectById(req, res, next){
+        try {
+            const owner = req.user._id;
+            const projectID = req.params.id;
+            const project = await this.findProject(projectID, owner)
+            return res.status(200).json({
+                status: 200,
+                success: true,
+                project
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+    async removeProject(req, res, next){
+        try {
+            const owner = req.user._id;
+            const projectID = req.params.id;
+            await this.findProject(projectID, owner)
+            const deleteProject = await ProjectModel.deleteOne({_id : projectID});
+            if(deleteProject.deletedCount == 0) throw {status: 400, message: "پروژه حذف نشد"};
+            return res.status(200).json({
+                status: 200,
+                success: true,
+                message: "پروژه با موفقیت حذف گردید"
+            })
+        } catch (error) {
+            next(error)
+        }
     }
     getAllProjectOfTeam(){
         
@@ -39,9 +71,6 @@ class ProjectConttroller{
 
     }
     updateProject(){
-
-    }
-    removeProject(){
 
     }
 }
