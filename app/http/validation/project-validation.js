e7aa1daa4f3d4fcc8a177e01bd6de4947a7fd568
mehdiqor <mehdighorbanin@gmail.com>
2022-11-19 18:40:00 +0330

    create project

--- app/http/validation/project-validation.js
+++ app/http/validation/project-validation.js
@@ -3,6 +3,7 @@ const { body } = require("express-validator");
 function createProjectValidator(){
     return [
         body("title").notEmpty().withMessage("عنوان پروژه نمیتواند خالی باشد"),
+        body("tags").isArray({min: 0, max: 10}).withMessage("حداکثر هشتگ ها باید 10 عدد باشد"),
         body("text").notEmpty().isLength({min : 20}).withMessage("توضیحات پروژه نمیتواند خالی باشد و حداقل باید 20 نویسه باشد")
     ]
 }
