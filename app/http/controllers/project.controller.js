6ff5cf3a667ec86af351f0f276d045de9be8507a
mehdiqor <mehdighorbanin@gmail.com>
2022-11-20 17:45:37 +0330

    project section2

--- app/http/controllers/project.controller.js
+++ app/http/controllers/project.controller.js
@@ -1,6 +1,10 @@
 const { ProjectModel } = require("../../models/project");
+const autobBind = require("auto-bind")
 
 class ProjectConttroller{
+    constructor(){
+        autobBind(this)
+    }
     async createProject(req, res, next){
         try {
             const {title, text, image, tags} = req.body;
@@ -17,11 +21,53 @@ class ProjectConttroller{
             next(error)
         }
     }
-    getAllProject(){
-
+    async getAllProject(req, res, next){
+        try {
+            const owner = req.user._id;
+            const projects = await ProjectModel.find({owner});
+            return res.status(200).json({
+                status: 200,
+                success: true,
+                projects
+            })
+        } catch (error) {
+            next(error)
+        }
     }
-    getProjectById(){
-
+    async findProject(projectId, owner){
+        const project = await ProjectModel.findOne({owner, _id : projectId});
+        if(!project) throw {status: 404, message: "پروژه ای یافت نشد"};
+        return project
+    }
+    async getProjectById(req, res, next){
+        try {
+            const owner = req.user._id;
+            const projectId = req.params.id;
+            const project = await this.findProject(projectId, owner)
+            return res.status(200).json({
+                status: 200,
+                success: true,
+                project
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+    async removeProject(req, res, next){
+        try {
+            const owner = req.user._id;
+            const projectId = req.params.id;
+            await this.findProject(projectId, owner)
+            const deleteProject = await ProjectModel.deleteOne({_id : projectId})
+            if(deleteProject.deletedCount == 0) throw {status: 400, message: "پروژه حذف نشد"}
+            return res.status(200).json({
+                status: 200,
+                success: true,
+                message: "پروژه با موفقیت حذف گردید"
+            })
+        } catch (error) {
+            next(error)
+        }
     }
     getAllProjectOfTeam(){
         
@@ -30,9 +76,6 @@ class ProjectConttroller{
 
     }
     updateProject(){
-
-    }
-    removeProject(){
 
     }
 }
--- app/router/project-router.js
+++ app/router/project-router.js
@@ -4,9 +4,14 @@ const { expressValidatorMapper } = require("../http/middlewares/checkErrors");
 const { createProjectValidator } = require("../http/validation/project-validation");
 const fileUpload = require("express-fileupload");
 const { uploadFile } = require("../modules/express-fileupload");
+const { mongoIdValidator } = require("../http/validation/public");
 
 const router = require("express").Router();
 router.post("/create", fileUpload(), checkLogin, uploadFile, createProjectValidator(), expressValidatorMapper, ProjectConttroller.createProject)
+router.get("/list", fileUpload(), checkLogin, ProjectConttroller.getAllProject)
+router.get("/:id", fileUpload(), checkLogin, mongoIdValidator(), expressValidatorMapper, ProjectConttroller.getProjectById)
+router.delete("/remove/:id", fileUpload(), checkLogin, mongoIdValidator(), expressValidatorMapper, ProjectConttroller.removeProject)
+router.put("/edit/:id", fileUpload(), checkLogin, mongoIdValidator(), expressValidatorMapper, ProjectConttroller.updateProject)
 
 module.exports = {
     projectRoutes : router
